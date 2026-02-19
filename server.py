123b48b0ac862336d747717232ee78ceea5a0890
VecherDobry <egorverche@gmail.com>
2026-02-19 22:37:20 +0300

    Update server.py

--- server.py
+++ server.py
@@ -13,6 +13,8 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 # Хранилище сообщений (последние 50)
 messages = []
+# Хранилище пользователей
+users = {}  # {socket_id: {'name': username, 'status': 'online'}}
 
 @app.route('/')
 def index():
@@ -25,28 +27,80 @@ def static_files(path):
 @socketio.on('connect')
 def handle_connect():
     print('Клиент подключился')
-    # Отправляем историю сообщений новому пользователю
-    for msg in messages[-50:]:
-        emit('message', msg)
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    print('Клиент отключился')
+    print(f'Клиент {users.get(request.sid, {}).get("name", "Unknown")} отключился')
+    if request.sid in users:
+        # Удаляем пользователя
+        del users[request.sid]
+        # Отправляем обновленный список пользователей всем
+        emit('users_update', [{'id': sid, 'name': data['name'], 'status': data['status']} 
+                              for sid, data in users.items()], broadcast=True)
+
+@socketio.on('register_user')
+def handle_register(data):
+    username = data.get('username', 'Аноним')
+    print(f'Пользователь зарегистрирован: {username} (ID: {request.sid})')
+    
+    # Сохраняем пользователя
+    users[request.sid] = {
+        'name': username,
+        'status': 'online'
+    }
+    
+    # Отправляем подтверждение регистрации
+    emit('registration_success', {'id': request.sid, 'name': username})
+    
+    # Отправляем список всех пользователей новому пользователю
+    emit('users_update', [{'id': sid, 'name': data['name'], 'status': data['status']} 
+                          for sid, data in users.items()])
+    
+    # Отправляем историю сообщений
+    for msg in messages[-50:]:
+        emit('message', msg)
+    
+    # Оповещаем всех о новом пользователе
+    emit('user_joined', {'id': request.sid, 'name': username}, broadcast=True)
 
 @socketio.on('message')
 def handle_message(data):
-    print(f'Получено сообщение: {data}')
+    print(f'Сообщение от {data.get("senderName")}: {data.get("text")}')
+    
+    # Добавляем информацию об отправителе
+    if 'senderId' not in data:
+        data['senderId'] = request.sid
+    
     # Добавляем в историю
     messages.append(data)
     # Ограничиваем историю 100 сообщениями
     if len(messages) > 100:
         messages.pop(0)
-    # Отправляем всем
-    emit('message', data, broadcast=True)
+    
+    # Отправляем сообщение конкретному получателю или всем
+    if data.get('recipient') and data['recipient'] != 'всем':
+        # Отправляем личное сообщение
+        recipient_id = data.get('recipientId')
+        if recipient_id:
+            emit('message', data, room=recipient_id)
+        emit('message', data, room=request.sid)  # Отправляем копию отправителю
+    else:
+        # Отправляем всем (общий чат)
+        emit('message', data, broadcast=True)
+
+@socketio.on('typing')
+def handle_typing(data):
+    """Обработка индикатора набора текста"""
+    recipient_id = data.get('recipientId')
+    if recipient_id:
+        emit('typing', {
+            'userId': request.sid,
+            'username': users.get(request.sid, {}).get('name', 'Неизвестно'),
+            'isTyping': data.get('isTyping', True)
+        }, room=recipient_id)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     print(f"Сервер запускается на порту {port}")
     print(f"Текущая директория: {os.getcwd()}")
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
-
